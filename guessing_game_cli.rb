09b17ebdcf2392027fae7ad573cad0374ec39b9e
Mustafa Muse <umpp101@icloud.com>
2019-09-17 21:19:36 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,4 +1,5 @@
 def run_guessing_game
+<<<<<<< HEAD
     randomized_number = rand(6) + 1
     user_input = gets.chomp
     if user_input.to_i == randomized_number
@@ -12,3 +13,17 @@ def run_guessing_game
   
   
   
+=======
+  randomized_number = rand(6) + 1
+  user_input = gets.chomp
+  if user_input.to_i == randomized_number
+    puts  "You guessed the correct number!"
+  elsif user_input == "exit"
+    puts "Goodbye!"
+  else 
+    puts "Sorry! The computer guessed #{randomized_number}."
+  end
+end
+
+
+>>>>>>> 927e34dbce02e1a17233f7378763bae2ffdf2f95
